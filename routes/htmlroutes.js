55c4eb6d8a93ab13a61908db5561f9d146564032
CalumR23 <searagan@gmail.com>
2019-12-05 20:30:42 -0800

    added server and htmlroutes code

---
+++ routes/htmlroutes.js
@@ -0,0 +1,25 @@
+var path = require("path");
+
+module.exports = function (app) {
+    app.get("/contact", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/contact.html"));
+    });
+
+    app.get("/reserve", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/about.html"));
+    });
+
+    app.get("/jobs", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/jobs.html"));
+    });
+    
+    app.get("/services", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/services.html"));
+    });
+
+
+    // If no matching route is found default to home
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+};
